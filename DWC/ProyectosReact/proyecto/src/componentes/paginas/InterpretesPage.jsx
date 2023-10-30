4e2707354dab490c98b88c5cf742fc8bcdd68de1
Sergio Alfonso <sergioalfonso.alu@iespacomolla.es>
2023-10-30 16:55:12 +0100

    Cambios (ns de q)

---
+++ DWC/ProyectosReact/proyecto/src/componentes/paginas/InterpretesPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './InterpretesPage.css';
+
+const InterpretesPage = () => {
+  return (
+    <div className="InterpretesPage-container">
+      <div className='InterpretesPage'>
+        <h1>Intérpretes</h1>
+        <p>Información sobre los actores y actrices de las películas.</p>
+      </div>
+    </div>
+  )
+}
+
+export default InterpretesPage;
