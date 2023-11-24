9c19ac77567f6935cd3095104b173baab1d8e386
Sergio Alfonso <sergioalfonso.alu@iespacomolla.es>
2023-11-24 19:57:55 +0100

    Actividades Rosa y Práctica useEffect React

--- DWC/ProyectosReact/proyecto/src/App.jsx
+++ DWC/ProyectosReact/proyecto/src/App.jsx
@@ -2,13 +2,14 @@ import { Fragment } from 'react';
 import Localizador from './componentes/EJ1/Localizador';
 import Colores from './componentes/EJ2/Colores';
 import Cronometro from './componentes/EJ3/Cronometro';
+import './App.css';
 
 const App = () => {
   return (
     <Fragment>
       {/* <Localizador /> */}
-      <Colores />
-      {/* Cronometro */}
+      {/* <Colores /> */}
+      <Cronometro />
     </Fragment>
   );
 };
